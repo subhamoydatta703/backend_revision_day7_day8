d85fdf1c9fd8fe8504197d0fabe24a8374f89e72
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-24 12:24:16 +0530

    Adding ejs

--- index.js
+++ index.js
@@ -10,8 +10,37 @@ app.set("views", path.join(__dirname, "views"));
 app.set(express.static(path.join(__dirname, "views")));
 
 
+let posts = [
+  {
+    username: "Rahul",
+    content: "I love coding!"
+  },
+  {
+    username: "Alice",
+    content: "JavaScript makes web development so much fun!"
+  },
+  {
+    username: "Bob",
+    content: "Node.js is perfect for building RESTful APIs."
+  },
+  {
+    username: "Charlie",
+    content: "I am currently learning Express and EJS templating."
+  },
+  {
+    username: "Subhamoy",
+    content: "Consistency is the key to mastering programming."
+  }
+];
+
+
+
 app.get("/",(req, res)=>{
     res.send("Working well");
+})
+app.get("/posts",(req, res)=>{
+
+    res.render("index.ejs", {posts});
 })
 
 
