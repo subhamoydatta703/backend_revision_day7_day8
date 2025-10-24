df0cd9936ec7c9b17d99b83c97d4390935963efc
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-24 21:29:17 +0530

    Using uuid

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const { v4: uuidv4 } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -9,70 +10,62 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public")));
 
-
 let posts = [
   {
-    id:"1a",
+    id: uuidv4(),
     username: "Rahul",
-    content: "I love coding!"
+    content: "I love coding!",
   },
   {
-    id:"1b",
+    id: uuidv4(),
     username: "Alice",
-    content: "JavaScript makes web development so much fun!"
+    content: "JavaScript makes web development so much fun!",
   },
   {
-    id:"1c",
+    id: uuidv4(),
     username: "Bob",
-    content: "Node.js is perfect for building RESTful APIs."
+    content: "Node.js is perfect for building RESTful APIs.",
   },
   {
-    id:"1d",
+    id: uuidv4(),
     username: "Charlie",
-    content: "I am currently learning Express and EJS templating."
+    content: "I am currently learning Express and EJS templating.",
   },
   {
-    id:"1e",
+    id: uuidv4(),
     username: "Subhamoy",
-    content: "Consistency is the key to mastering programming."
-  }
+    content: "Consistency is the key to mastering programming.",
+  },
 ];
 
+app.get("/", (req, res) => {
+  // res.send("Working well");
+  res.redirect("/posts");
+});
+app.get("/posts", (req, res) => {
+  res.render("index.ejs", { posts });
+});
+
+app.get("/posts/new", (req, res) => {
+  res.render("new.ejs");
+});
+
+app.get("/posts/:id", (req, res) => {
+  let { id } = req.params;
+  let post = posts.find((p) => id === p.id);
+  
+  console.log(post);
 
 
-app.get("/",(req, res)=>{
-    // res.send("Working well");
-    res.redirect("/posts")
-})
-app.get("/posts",(req, res)=>{
+  res.render("show.ejs", { post });
+});
 
-    res.render("index.ejs", {posts});
-})
-
-
-app.get("/posts/:id", (req, res)=>{
-    let { id }=req.params;
-    let post = posts.find(p => id === p.id)
-    console.log(post);
-    console.log("P id: ", post.id);
-    console.log("P username: ", post.username);
-    console.log("P content: ", post.content);
-
-    res.render("show.ejs",{ post});
-})
-
-
-app.get("/posts/new", (req, res)=>{
-    res.render("new.ejs")
-})
-
-
-app.post("/posts", (req, res)=>{
-   let { username, content } = req.body;
-   posts.push({username, content})
-   res.redirect("/posts")
-   
-})
+app.post("/posts", (req, res) => {
+  let { username, content } = req.body;
+  let id = uuidv4();
+  posts.push({ id, username, content });
+   res.redirect(`/posts/${id}`);
+});
 
 port = 8080;
 
