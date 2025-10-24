94a77939df37e1dfeed0a1970e34165cc6ef815e
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-24 12:07:08 +0530

    Initial server setup

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 const express = require("express");
+const path = require("path");
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.set(express.static(path.join(__dirname, "views")));
+
+
+app.get("/",(req, res)=>{
+    res.send("Working well");
+})
+
+
+port = 8080;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
