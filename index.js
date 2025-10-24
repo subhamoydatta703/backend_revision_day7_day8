fcf22c5caa31daaf77a08697fe8b73511c389c86
Subhamoy Datta <subhamoydatta99@gmail.com>
2025-10-24 12:32:26 +0530

    Adding css

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "views")));
+app.use(express.static(path.join(__dirname, "public")));
 
 
 let posts = [
